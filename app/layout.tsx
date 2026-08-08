ebd53dd2cf57faadc52193bc7e73e32f9301bb81
d <dakshamin15@gmail.com>
2026-08-07 23:35:23 -0400

    Change icon size from 100x100 to 50x50

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     icon: [
       {
         url: "/busguard.png",
-        sizes: "100x100",
+        sizes: "50x50",
         type: "image/png",
       },
     ],
